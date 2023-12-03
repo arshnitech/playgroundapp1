15396131070ecae1616a48e709e9916a36a36675
arshnitech <124500205+arshnitech@users.noreply.github.com>
2023-12-03 22:42:24 +0530

    Update auth.js

--- azureauth/auth.js
+++ azureauth/auth.js
@@ -1,14 +1,7 @@
 const msal = require('@azure/msal-node');
 const jwt = require('jsonwebtoken');
 /* Credentials */
-//TENANT_ID="4ea7b175-bdf9-4493-bbb8-8a47c530676c"
-//CLIENT_ID="8e3291a3-8063-4d25-95dc-60fb4b549125"
-//CLIENT_ID="034dc914-47ac-4a86-9ab8-36bf6de2712b"
-//CLIENT_SECRET="pnu8Q~X8wsnFXNkybIcn_7VaibXBLaoD2Tda6akT"
-//CLIENT_SECRET="T~98Q~kNce1IflfEbh-kaG5MewU5ucwgdfABvbfv"
-/* Endpoints */
-//AAD_ENDPOINT="https://login.microsoftonline.com"
-//GRAPH_ENDPOINT="https://graph.microsoft.com"
+
 /**
  * Configuration object to be passed to MSAL instance on creation.
  * For a full list of MSAL Node configuration parameters, visit:
@@ -16,11 +9,11 @@ const jwt = require('jsonwebtoken');
  */
 const msalConfig = {
     auth: {
-        //clientId: '6c734437-2662-4867-86c2-dbf17f80d9ad',
-        clientId: '78d6c944-fc6d-45a4-bd89-5d975fb3a102',
-        authority: 'https://login.microsoftonline.com/4ea7b175-bdf9-4493-bbb8-8a47c530676c',
-        //clientSecret: '1F28Q~8e7SQWZtM27IQlYZLvMa1EInzwmg2Gfc2H',
-        clientSecret:'L3W8Q~NcVucAhdV8fZ2VoTAqVbqaEWRLhzMJlbsa'
+      
+        clientId: '702',
+        authority: 'https://login.microsoftonline.com/4,
+     
+        clientSecret:'L3a'
     }
 };
 
